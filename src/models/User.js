dde232c36aafc32eedc0d1b988cfb7fb8562da8b
Smita Deshpande <smitadeshpande@Smitas-MacBook-Pro.local>
2021-03-19 15:15:47 -0400

    auth routes created

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt =require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -14,5 +15,42 @@ const userSchema = new mongoose.Schema({
     }
 
 })
+
+userSchema.pre('save',function(next){
+    const user =this
+    if(!user.isModified('password')){
+        return next()
+    }
+    bcrypt.genSalt(10,(error,salt)=>{
+        if(error){
+            return next(error)
+        }
+        bcrypt.hash(user.password,salt,(error,hash)=>{
+            if(error){
+                return next(error)
+            }
+            user.password =hash
+            next()
+        })
+    })
+
+})
+
+userSchema.methods.comparePassword =function(candidatePassword){
+    const user = this
+    return new Promise((resolve,reject) =>{
+        bcrypt.compare(candidatePassword,user.password,(error,isMatch)=>{
+            if(error){
+                return reject(error)
+            }
+            if(!isMatch){
+                return reject(error)
+            }
+            resolve(true)
+        })
+
+    })
+
+}
 
 mongoose.model('User',userSchema)
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,29 +1,52 @@
 const express = require('express')
-const mongoose  = require('mongoose')
+const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
-const{jwtkeys} =require("./keys")
+const { jwtkeys } = require("./keys")
 const router = express.Router()
 
 
-const User= mongoose.model('User')
+const User = mongoose.model('User')
 
 
 
-router.post('/signup', async(req,res) =>{
-    console.log(req.body)
+router.post('/signup', async (req, res) => {
+  console.log(req.body)
 
-    const{email,password} = req.body
+  const { email, password } = req.body
 
-    try {
-        const user = new User({email,password})
-         await user.save()
-         const token = jwt.sign({userId:user._id},jwtkeys)
-          res.send({token})
-    } catch (err) {
-        res.status(422).send(err.message)
-        
-    }
-   
-  })
+  try {
+    const user = new User({ email, password })
+    await user.save()
+    const token = jwt.sign({ userId: user._id }, jwtkeys)
+    res.send({ token })
+  } catch (err) {
+    res.status(422).send(err.message)
 
-module.exports =router
+  }
+
+})
+
+router.post('/signin',async(req,res) =>{
+  const{email,password} = req.body
+  if(!email || !password){
+    return res.status(422).send({error:"must provide email and password"})
+  }
+  const user =  await User.findOne({email})
+  if(!user){
+    return res.status(422).send({error:"must provide email and password"})
+
+  }
+
+  try {
+    await user.comparePassword(password)
+    const token = jwt.sign({ userId: user._id }, jwtkeys)
+    res.send({ token })
+    
+  } catch (error) {
+    return res.status(422).send({error:"must provide email and password"})
+    
+  }
+ 
+})
+
+module.exports = router
